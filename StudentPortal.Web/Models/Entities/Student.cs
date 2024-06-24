a84c593a1365df2e212a44f8da1e65b6260e6e14
PERCOID-IT <PERCOID-IT@DESKTOP-SANISHA>
2024-06-24 15:30:02 +0545

    Add project files.

---
+++ StudentPortal.Web/Models/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+    public class Student
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
