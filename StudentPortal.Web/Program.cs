5153e47859d7d70a408e65e1754bccb77f5b577a
PERCOID-IT <PERCOID-IT@DESKTOP-SANISHA>
2024-06-24 15:21:07 +0545

    Initial Migration#

--- StudentPortal.Web/Program.cs
+++ StudentPortal.Web/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// To inject DbContext in this application  we have to  defined in the service conteainer
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal")));
+
 
 var app = builder.Build();
 
